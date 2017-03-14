4cbf6bc80c9eb3573db2f279b45b3227dc20476a
master wang <JulyMars@users.noreply.github.com>
2017-03-14 15:43:25 +0800

    svn更新提交脚本

---
+++ 提交.bat
@@ -0,0 +1,5 @@
+:提交SVN BB客户端目录
+"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /command:commit /path:"D:\work\awf" /closeonend:0
+
+echo 提交成功！
+pause
---
+++ 更新src-resource.bat
@@ -0,0 +1,8 @@
+:更新SVN BB客户端目录
+"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /command:update /path:"D:\work\awf\src" /closeonend:0
+
+:更新SVN BB策划目录
+"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /command:update /path:"D:\work\awf\resource" /closeonend:0
+
+echo 更新成功！
+pause
